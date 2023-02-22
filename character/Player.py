00bfc0ca516c146fe20604dc8eae994bb54e85db
unknown <daniel.lorincz@lightyear.one>
2023-02-22 09:18:38 +0100

    Modifications on drawing and character abstract class added

---
+++ character/Player.py
@@ -0,0 +1,72 @@
+import pygame
+from lib.definitions import *
+
+from character.Character import Character
+class Player(Character):
+    def __init__(self):
+        self.pos_x = 8
+        self.pos_y = 0
+        self.image = CHARACTER_NORMAL
+        self.move_index = 0
+        self.attack_index = 0
+
+    def _move(self, collision_points): 
+        key_input = pygame.key.get_pressed()
+        character_idle = False
+        if key_input[pygame.K_LEFT]:
+            self.pos_y -= 1
+            if(self._collide(collision_points, self.pos_x, self.pos_y) or self.pos_y < 0):
+                self.pos_y += 1
+            self.move_index += 1
+        elif key_input[pygame.K_RIGHT]:
+            self.pos_y += 1
+            if(self._collide(collision_points, self.pos_x, self.pos_y)):
+                self.pos_y -= 1
+            self.move_index += 1
+        elif key_input[pygame.K_UP]:
+            self.pos_x -= 1
+            if(self._collide(collision_points, self.pos_x, self.pos_y) or self.pos_x < 0):
+                self.pos_x += 1
+            self.move_index += 1
+        elif key_input[pygame.K_DOWN]:
+            self.pos_x += 1
+            if(self._collide(collision_points, self.pos_x, self.pos_y)):
+                self.pos_x -= 1
+            self.move_index += 1
+        else:
+            character_idle = True
+        if self.move_index > 5:
+            self.move_index = 0
+        if character_idle:
+            self.image = CHARACTER_NORMAL
+        else:
+            self.image = CHARACTER_WALK[self.move_index]
+
+        return self.pos_x, self.pos_y
+    
+    def _collide(self, collision_points, x, y):
+        for x_i in range(len(collision_points[0])):
+            if collision_points[0][x_i] == x and collision_points[1][x_i] == y:
+                print("Collide")
+                return True
+        return False
+    
+    def _attack(self):
+        key_input = pygame.key.get_pressed()
+        if(key_input[pygame.K_a]):
+            if self.attack_index > 2:
+                self.attack_index = 0
+            self.image = CHARACTER_ATTACK[self.attack_index]
+            self.attack_index += 1
+
+    def character_action(self, collision_points):
+        self._move(collision_points)
+        self._attack()
+
+            
+
+
+        
+
+
+            
--- main.py
+++ main.py
@@ -2,18 +2,18 @@ import pygame
 import sys
 import numpy as np
 from lib.definitions import *
-from character.Character import Character
+from character.Player import Player
 
 
 def main():
     global SCREEN, CLOCK
     pygame.init()
-    SCREEN = pygame.display.set_mode((WINDOW_WIDTH, 500))
+    SCREEN = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
     CLOCK = pygame.time.Clock()
-    character = Character()
+    player = Player()
     
     while True:
-        drawGrid(character)
+        drawGrid(player)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -23,12 +23,11 @@ def main():
         CLOCK.tick(FPS)
 
 
-def drawGrid(character):
+def drawGrid(player):
     blockSize = BLOCK_SIZE #Set the size of the grid block
     map_arr = np.array(MAP)
     collision_points = np.where(map_arr=='O')
-    char_x, char_y = character.move(collision_points)
-    character.attack()
+    player.character_action(collision_points)
     for x in range(0, WINDOW_WIDTH, blockSize):
         for y in range(0, WINDOW_HEIGHT, blockSize):
             x_index = int(x / blockSize)
@@ -37,9 +36,14 @@ def drawGrid(character):
                 rect = pygame.image.load(NORMAL_TILE[0]).convert()
             elif MAP[x_index][y_index] == 'X':
                 rect = pygame.image.load(ROAD_TILE).convert()
-            if x_index == char_x and y_index == char_y:
-                rect = pygame.image.load(character.image).convert()
             RECT_SIZED = pygame.transform.scale(rect, (BLOCK_SIZE, BLOCK_SIZE))
             SCREEN.blit(RECT_SIZED, (y,x))
+
+            if x_index == player.pos_x and y_index == player.pos_y:
+                char = pygame.image.load(player.image)
+                CHAR_SIZED = pygame.transform.scale(char, (BLOCK_SIZE, BLOCK_SIZE))
+                SCREEN.blit(CHAR_SIZED, (y,x))
+                
+
 
 main()
